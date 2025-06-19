ad76ceb9b1c5703753ce883cb9ab2f3662d512f1
Louchart Sebastien <sebastien.louchart.itp@gmail.com>
2025-06-19 08:48:29 +0200

    rename variables in runtime.loops

--- pyforth/runtime/loops.py
+++ pyforth/runtime/loops.py
@@ -9,8 +9,8 @@ from pyforth.runtime.utils import compiling_word, fatal, define_word
 def xt_c_begin(state: State) -> None:
     if not state.is_compiling:
         fatal("BEGIN: not in compile mode")
-    slot = state.compile_to_current_definition()
-    state.control_stack.append(("BEGIN", slot))  # flag for following UNTIL/REPEAT
+    begin_pos: POINTER = state.compile_to_current_definition()
+    state.control_stack.append(("BEGIN", begin_pos))  # flag for following UNTIL/REPEAT
 
 
 @define_word("until")
@@ -20,14 +20,14 @@ def xt_c_until(state: State) -> None:
         fatal("UNTIL: not in compile mode")
     if not state.control_stack:
         fatal("No BEGIN for UNTIL to match")
-    word, slot = state.control_stack.pop()
-    if word != "BEGIN":
-        fatal(f"UNTIL preceded by {word} (not BEGIN)")
+    cs_word, begin_pos = state.control_stack.pop()
+    if cs_word != "BEGIN":
+        fatal(f"UNTIL preceded by {cs_word} (not BEGIN)")
 
     state.compile_to_current_definition(
         [
             primitives.xt_r_jz,
-            slot
+            begin_pos
         ]
     )
 
@@ -38,8 +38,8 @@ def xt_c_while(state: State) -> None:
     if not state.is_compiling:
         fatal("WHILE: not in compile mode")
 
-    ptr = state.compile_to_current_definition(primitives.xt_r_jz)
-    state.control_stack.append(("WHILE", ptr))  # flag for following REPEAT
+    while_pos: POINTER = state.compile_to_current_definition(primitives.xt_r_jz)
+    state.control_stack.append(("WHILE", while_pos))  # flag for following REPEAT
     state.compile_to_current_definition(0)  # to be filled in by REPEAT
 
 
@@ -51,25 +51,25 @@ def xt_c_repeat(state: State) -> None:
         fatal("REPEAT: not in compile mode")
     if not state.control_stack:
         fatal("No WHILE for REPEAT to match")
-    word, slot2 = state.control_stack.pop()
-    if word != "WHILE":
-        fatal(f"REPEAT preceded by {word} (not WHILE)")
-    assert isinstance(slot2, POINTER)
+    cs_word, while_pos = state.control_stack.pop()
+    if cs_word != "WHILE":
+        fatal(f"REPEAT preceded by {cs_word} (not WHILE)")
+    assert isinstance(while_pos, POINTER)
 
     if not state.control_stack:
         fatal('No BEGIN for REPEAT to match')
 
-    word, slot1 = state.control_stack.pop()
-    if word != 'BEGIN':
-        fatal(f"WHILE-REPEAT preceded by {word} (not BEGIN)")
+    cs_word, begin_pos = state.control_stack.pop()
+    if cs_word != 'BEGIN':
+        fatal(f"WHILE-REPEAT preceded by {cs_word} (not BEGIN)")
 
     state.compile_to_current_definition(
         [
             primitives.xt_r_jmp,
-            slot1
+            begin_pos
         ]
     )
-    state.close_jump_address(slot2)  # close JNZ for WHILE
+    state.close_jump_address(while_pos)  # close JNZ for WHILE
 
 
 @define_word("again")
@@ -79,13 +79,13 @@ def xt_c_again(state: State) -> None:
         fatal("AGAIN: not in compile mode")
     if not state.control_stack:
         fatal("No BEGIN for AGAIN to match")
-    word, slot = state.control_stack.pop()
-    if word != "BEGIN":
-        fatal(f"AGAIN preceded by {word} (not BEGIN)")
+    cs_word, begin_pos = state.control_stack.pop()
+    if cs_word != "BEGIN":
+        fatal(f"AGAIN preceded by {cs_word} (not BEGIN)")
 
     state.compile_to_current_definition(
         [
             primitives.xt_r_jmp,
-            slot
+            begin_pos
         ]
     )
