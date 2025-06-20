26d91c99b72a7f0c0a32b90a55e75388c509d3c1
Louchart Sebastien <sebastien.louchart.itp@gmail.com>
2025-06-20 10:03:22 +0200

    add some comments to guide replacing control-struct primitives by defined words

--- pyforth/runtime/branching.py
+++ pyforth/runtime/branching.py
@@ -15,10 +15,14 @@ def xt_c_if(_: State, compiler: Compiler) -> None:
 @define_word('else')
 @compiling_word
 def xt_c_else(_: State, compiler: Compiler) -> None:
+    # as AHEAD (TBD)
     compiler.compile_to_current_definition(primitives.xt_r_jmp)
     compiler.control_structure_init_open_orig()
 
+    # as 1 CS-ROLL
     compiler.control_stack_roll(1)  # swap CS TOS & NOS
+
+    # as POSTPONE THEN
     compiler.control_struct_close_open_orig() # close JZ for IF
 
 
--- pyforth/runtime/loops.py
+++ pyforth/runtime/loops.py
@@ -22,16 +22,23 @@ def xt_c_until(_: State, compiler: Compiler) -> None:
 @define_word("while")
 @compiling_word
 def xt_c_while(_: State, compiler: Compiler) -> None:
+    # as POSTPONE IF
     compiler.compile_to_current_definition(primitives.xt_r_jz)
     compiler.control_structure_init_open_orig()
+
+    # as 1 CS-ROLL
     compiler.control_stack_roll(1)
 
 
 @define_word("repeat")
 @compiling_word
 def xt_c_repeat(_: State, compiler: Compiler) -> None:
+
+    # as POSTPONE AGAIN
     compiler.compile_to_current_definition(primitives.xt_r_jmp)
     compiler.control_structure_close_open_dest()
+
+    # as a new word TBD
     compiler.control_struct_close_open_orig()  # close JNZ for WHILE
 
 
