ff7cc922afbb7c383dc360a9c4e2617e455f1934
Louchart Sebastien <sebastien.louchart.itp@gmail.com>
2025-06-20 09:32:14 +0200

    rationalize CS code with compiler primitives (loops)

--- pyforth/runtime/loops.py
+++ pyforth/runtime/loops.py
@@ -9,42 +9,29 @@ from pyforth.runtime.utils import compiling_word, fatal, define_word
 @define_word("begin")
 @compiling_word
 def xt_c_begin(_: State, compiler: Compiler) -> None:
-    dest: POINTER = compiler.compile_to_current_definition()
-    compiler.control_stack.append(dest)  # flag for following UNTIL/REPEAT
+    compiler.control_structure_init_open_dest()
 
 
 @define_word("until")
 @compiling_word
 def xt_c_until(_: State, compiler: Compiler) -> None:
-    dest = compiler.control_stack.pop()
-    compiler.compile_to_current_definition(
-        [
-            primitives.xt_r_jz,
-            dest
-        ]
-    )
+    compiler.compile_to_current_definition(primitives.xt_r_jz)
+    compiler.control_structure_close_open_dest()
 
 
 @define_word("while")
 @compiling_word
 def xt_c_while(_: State, compiler: Compiler) -> None:
-    dest: POINTER = cast(POINTER, compiler.control_stack.pop())
-    orig: POINTER = compiler.compile_to_current_definition(primitives.xt_r_jz)
-    compiler.control_stack.append(orig)  # flag for following REPEAT
-    compiler.compile_to_current_definition(0)  # to be filled in by REPEAT
-    compiler.control_stack.append(dest)
+    compiler.compile_to_current_definition(primitives.xt_r_jz)
+    compiler.control_structure_init_open_orig()
+    compiler.control_stack_roll(1)
 
 
 @define_word("repeat")
 @compiling_word
 def xt_c_repeat(_: State, compiler: Compiler) -> None:
-    dest: POINTER = cast(POINTER, compiler.control_stack.pop())
-    compiler.compile_to_current_definition(
-        [
-            primitives.xt_r_jmp,
-            dest
-        ]
-    )
+    compiler.compile_to_current_definition(primitives.xt_r_jmp)
+    compiler.control_structure_close_open_dest()
     compiler.control_struct_close_open_orig()  # close JNZ for WHILE
 
 
